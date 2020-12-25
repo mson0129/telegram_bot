e2ff6b37edd8c75cfafbf0230e982f5392419461
Michael Son <mson0129@gmail.com>
2020-12-25 21:04:22 +0900

    Update a7s3.py

--- ward/a7s3.py
+++ ward/a7s3.py
@@ -39,27 +39,27 @@ else:
     body = "입고가 완료되었습니다! 지금 바로 구매하세요."
     disable_notification = False
 
-#텔레그램 메시지 보내기
-#버튼 생성
-keyboard = [
-    [telegram.InlineKeyboardButton(text="사이트로 이동", url=url)]
-]
-text = "<b>{target}: {title}</b>\n{body}\n".format(target=target, title=title, body=body)
-chat_token = os.environ["CHAT_BOT_TOKEN"]
-chat_id = os.environ["CHAT_USER_ID"]
-bot = telegram.Bot(token = chat_token)
-bot.send_message(
-    chat_id = chat_id,
-    text = text,
-    parse_mode="HTML",
-    disable_notification=disable_notification,
-    reply_markup=telegram.InlineKeyboardMarkup(keyboard)
-)
+    #텔레그램 메시지 보내기
+    #버튼 생성
+    keyboard = [
+        [telegram.InlineKeyboardButton(text="사이트로 이동", url=url)]
+    ]
+    text = "<b>{target}: {title}</b>\n{body}\n{url}".format(target=target, title=title, body=body)
+    chat_token = os.environ["CHAT_BOT_TOKEN"]
+    chat_id = os.environ["CHAT_USER_ID"]
+    bot = telegram.Bot(token = chat_token)
+    bot.send_message(
+        chat_id = chat_id,
+        text = text,
+        parse_mode="HTML",
+        disable_notification=disable_notification,
+        reply_markup=telegram.InlineKeyboardMarkup(keyboard)
+    )
 
 #이슈 남기기
 g = Github(os.environ["MY_GITHUB_TOKEN"])
 repo = g.get_user().get_repo("telegram_bot")
-repo.create_issue(title="{target}: {title}".format(target=target, title=title), body=body)
+repo.create_issue(title="{target}: {title}".format(target=target, title=title), body="{body}\n{url}".format(body=body, url=url))
 #이슈 클로징
 open_issues = repo.get_issues(state='open')
 for issue in open_issues:
