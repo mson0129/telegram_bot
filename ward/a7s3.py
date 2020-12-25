84b369ed42b45a9c54f0df94f4ed4eda86de8927
Michael Son <mson0129@gmail.com>
2020-12-25 20:54:36 +0900

    Update a7s3.py

--- ward/a7s3.py
+++ ward/a7s3.py
@@ -30,23 +30,31 @@ parent_path = bsObject.find("p", class_="btnArea")
 links = parent_path.find_all("a")
 if links[1].text == "일시품절":
     #일시 품절
-    title = "<b>재고 없음</b>"
+    title = "재고 없음"
     body = "재고가 없습니다."
+    disable_notification = True
 else:
     #입고 완료
-    title = "<b>입고 완료</b>"
+    title = "입고 완료"
     body = "입고가 완료되었습니다! 지금 바로 구매하세요."
+    disable_notification = False
 
 #텔레그램 메시지 보내기
 #버튼 생성
 keyboard = [
     [telegram.InlineKeyboardButton(text="사이트로 이동", url=url)]
 ]
-text = "{target}: {title}\n{body}\n".format(target=target, title=title, body=body)
+text = "*{target}: {title}*\n{body}\n".format(target=target, title=title, body=body)
 chat_token = os.environ["CHAT_BOT_TOKEN"]
 chat_id = os.environ["CHAT_USER_ID"]
 bot = telegram.Bot(token = chat_token)
-bot.send_message(chat_id = chat_id, text = text, reply_markup=telegram.InlineKeyboardMarkup(keyboard))
+bot.send_message(
+    chat_id = chat_id,
+    text = text,
+    parse_mode="MarkdownV2",
+    disable_notification=disable_notification,
+    reply_markup=telegram.InlineKeyboardMarkup(keyboard)
+)
 
 #이슈 남기기
 g = Github(os.environ["MY_GITHUB_TOKEN"])
